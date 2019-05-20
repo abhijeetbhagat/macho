f2d13138baca851989a9dad259f0adc2887c1a98
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-05-21 00:02:36 +0530

    Fix RTP header parsing

--- src/rtp/depacketizer.cpp
+++ src/rtp/depacketizer.cpp
@@ -5,18 +5,20 @@ Depacketizer::Depacketizer(){
 
 RtpPacket Depacketizer::parse(const std::string& buf){
   RtpPacket packet;
-  auto packet_len = (uint16_t)buf[2] << 8 | (uint16_t)buf[3];
-  auto payload = buf.substr(4, packet_len - 4);
+  //The following check is only valid for embedded (interleaved) binary data
+  //if(buf[0] == 0x24){ //'$' means start of packet
+  //auto packet_len = (uint16_t)buf[2] << 8 | (uint16_t)buf[3];
+  auto payload = buf;
 
-  auto version = payload[0] & 0x80 != 0 ? 2 : 1;
-  auto padding = (payload[0] & 0x20) > 0;
-  auto extension = (payload[0] & 0x10) > 0;
+  auto version = (payload[0] & 0xc0) >> 6;
+  auto padding = (payload[0] & 0x20) >> 5;
+  auto extension = (payload[0] & 0x10) >> 4;
   auto cc = (payload[0] & 0xf);
-  auto marker = (payload[1] & 0x80) > 0;
+  auto marker = (payload[1] & 0x80) >> 7;
   auto payload_type = (payload[1] & 0x7f);
   auto seq_num = (uint16_t)payload[2] << 8 | (uint16_t)payload[3];
-  auto timestamp = (uint32_t)payload[4] << 24 | (uint32_t)payload[5] << 16 | (uint16_t)payload[6] << 8 | (uint16_t)payload[7];
-  auto ssrc = (uint32_t)payload[8] << 24 | (uint32_t)payload[9] << 16 | (uint16_t)payload[10] << 8 | (uint16_t)payload[11];
+  auto timestamp = (uint32_t)payload[4] << 24 | (uint32_t)payload[5] << 16 | (uint32_t)payload[6] << 8 | (uint32_t)payload[7];
+  auto ssrc = (uint32_t)payload[8] << 24 | (uint32_t)payload[9] << 16 | (uint32_t)payload[10] << 8 | (uint32_t)payload[11];
 
   packet.header = RtpPacket::RtpHeader{
     version = version,
@@ -24,7 +26,7 @@ RtpPacket Depacketizer::parse(const std::string& buf){
     extension = extension,
     cc = cc,
     marker = marker,
-    //ptype = payload_type,
+    payload_type = payload_type,
     seq_num = seq_num,
     timestamp = timestamp,
     ssrc = ssrc
