b156a589a607d18e902551c17341d379d6e8afad
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-05-21 00:01:43 +0530

    Add support for << to RtpPacket

--- include/RtpPacket.h
+++ include/RtpPacket.h
@@ -1,6 +1,8 @@
 #ifndef _RTPPACKET_H_
 #define _RTPPACKET_H_
 
+#include <ostream>
+
 // TODO: will this struct work on Win too?
 // according to
 // https://stackoverflow.com/questions/25345691/bit-fields-portability/25345750
@@ -12,11 +14,18 @@ struct RtpPacket {
     int extension; // : 1;
     int cc;// : 4;
     int marker;// : 1;
-    int ptype;// : 7;
+    int payload_type;// : 7;
     short seq_num;// : 16;
     uint32_t timestamp;
     uint32_t ssrc;
   } header;
+
+  friend std::ostream& operator<<(std::ostream& o, const RtpPacket& packet){
+    o << "version: " << packet.header.version << "\npadding: " << packet.header.padding  << "\nextension: " << packet.header.extension 
+      << "\ncc: " << packet.header.cc  << "\nmarker: " << packet.header.marker  << "\nptype: " << packet.header.payload_type  << "\nseq_num: " << packet.header.seq_num
+      << "\ntimestamp: " << packet.header.timestamp << "\nssrc: " << packet.header.ssrc << '\n';
+    return o;
+  }
 };
 
 #endif
