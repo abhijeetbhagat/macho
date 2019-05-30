c72947bdf68ef215884136f820d563ce6ac7510e
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-05-30 21:59:30 +0530

    Add skeletal epoller implementation and include for building

--- libcircinus/include/epoller.h
+++ libcircinus/include/epoller.h
@@ -1,11 +1,25 @@
 #ifndef _EPOLLER_H
 #define _EPOLLER_H
 
+#include "AbstractIOMuxer.h"
+#include <sys/epoll.h>
+#include <memory>
 // NOTE: The inspiration for the design of this class came from the Tango
 // library written in D.
-class EPoller {
+class EPoller : public AbstractIOMuxer{
 public:
-  Epoller();
+  EPoller();
+  ~EPoller() {}
+  void open(uint32_t size) = 0;
+  void close() = 0;
+  void subscribe(int fd, Events event) = 0;
+  void unsubscribe() = 0;
+  int wait() { return wait(std::chrono::milliseconds::max()); }
+  int wait(const std::chrono::milliseconds &) = 0;
+
+private:
+  int _epollfd;
+  std::unique_ptr<epoll_event[]> _events;
 };
 
 #endif
--- libcircinus/src/epoller.cpp
+++ libcircinus/src/epoller.cpp
@@ -1,3 +1,21 @@
 #include "../include/epoller.h"
+#include <chrono>
 
-EPoller::Epoller() {}
+EPoller::EPoller() : _epollfd(-1), _events(nullptr) {}
+
+void EPoller::open(uint32_t size){
+  _events.reset(new epoll_event[size]);
+}
+
+void EPoller::close(){
+}
+
+void EPoller::subscribe(int fd, Events event){
+}
+
+void EPoller::unsubscribe(){
+}
+
+int EPoller::wait(const std::chrono::milliseconds &duration){
+}
+
