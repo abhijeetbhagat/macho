98054afc58008d3ad39cf983a71bbe3cb03377ef
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-06-01 23:15:02 +0530

    Use AbstractIOMuxer instead of Poller in RTPSession

--- include/rtp/rtp_session.h
+++ include/rtp/rtp_session.h
@@ -2,12 +2,19 @@
 #define _RTPSESSION_H
 
 #include "../transport/UdpSocket.h"
+#include "../../libcircinus/include/AbstractIOMuxer.h"
+#include <memory>
 // Note: The design of this class is based on the RTP book by Colin Perkins.
 // It was made generic by me.
 
 // TODO abhi: Make this policy based class for choosing the transportation
 // medium
 class RTPSession {
+public:
+  RTPSession(std::unique_ptr<AbstractIOMuxer> io_muxer, const std::string &ip, uint16_t data_port, uint16_t rtcp_port,
+             uint16_t server_rtp_port, uint16_t server_rtcp_port);
+  void start();
+private:
   std::string ip;
   uint16_t rtp_data_port;
   uint16_t rtcp_data_port;
@@ -15,11 +22,7 @@ class RTPSession {
   std::unique_ptr<UdpSocket> _audio_rtcp_socket; // rtcp data listener
   std::unique_ptr<UdpSocket> _video_rtp_socket;  // rtp data listener
   std::unique_ptr<UdpSocket> _video_rtcp_socket; // rtcp data listener
-
-public:
-  RTPSession(const std::string &ip, uint16_t data_port, uint16_t rtcp_port,
-             uint16_t server_rtp_port, uint16_t server_rtcp_port);
-  void start();
+  std::unique_ptr<AbstractIOMuxer> _io_muxer;
 };
 
 #endif
--- src/rtp/rtp_session.cpp
+++ src/rtp/rtp_session.cpp
@@ -8,21 +8,26 @@
 #include <chrono>
 #include <sys/time.h>
 
-RTPSession::RTPSession(const std::string &ip, uint16_t data_port,
+RTPSession::RTPSession(std::unique_ptr<AbstractIOMuxer> io_muxer, const std::string &ip, uint16_t data_port,
                        uint16_t rtcp_port, uint16_t server_rtp_port,
                        uint16_t server_rtcp_port) {
+  _io_muxer = std::move(io_muxer);
   _video_rtp_socket = std::unique_ptr<UdpSocket>(new UdpSocket());
   _video_rtp_socket->set_blocking(false);
   _video_rtp_socket->bind_to(ip, data_port);
 
-  // TODO if we dont do this, then we dont receive any packets
+  // TODO if we dont do this, then we might not receive any packets
+  // Found that this is used to overcome NAT traversal problem where we are behind a NAT
+  // and so the server's streams can't reach us
   _video_rtp_socket->send_to(ip, server_rtp_port, "Îúíþ");
 
   _video_rtcp_socket = std::unique_ptr<UdpSocket>(new UdpSocket());
   _video_rtcp_socket->set_blocking(false);
   _video_rtcp_socket->bind_to(ip, rtcp_port);
 
-  // TODO if we dont do this, then we dont receive any packets
+  // TODO if we dont do this, then we might not receive any packets
+  // Found that this is used to overcome NAT traversal problem where we are behind a NAT
+  // and so the server's streams can't reach us
   _video_rtcp_socket->send_to(ip, server_rtcp_port, "Îúíþ");
 }
 
@@ -50,19 +55,19 @@ void RTPSession::start() {
   pollfds[1].fd = rtcp_sd;
   pollfds[1].events = POLLIN;
   */
-  Poller poller;
-  poller.open(2);
-  poller.subscribe(rtp_sd, Events::Read);
-  poller.subscribe(rtcp_sd, Events::Read);
+  //Poller poller;
+  _io_muxer->open(2);
+  _io_muxer->subscribe(rtp_sd, Events::Read);
+  _io_muxer->subscribe(rtcp_sd, Events::Read);
 
   while (true) {
     // memcpy(&working_set, &master_set, sizeof master_set);
     SPDLOG_INFO("waiting for packet...\n");
-    int rc = poller.wait(std::chrono::milliseconds{50000});
+    int rc = _io_muxer->wait(std::chrono::milliseconds{50000});
     if (rc <= 0) {
       continue;
     }
-    auto ready_set = poller.ready_set();
+    auto ready_set = _io_muxer->ready_set();
     for (int i = 0; i < ready_set.size(); i++) {
       // if(FD_ISSET(i, &working_set)){
       if (ready_set[i] == rtp_sd) { // RTP socket ready to be read
