a0d861c5a7257c9128b4730915391f6c591d5578
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-06-03 22:01:45 +0530

    Add an input queue and depacketizer to h264 source filter

--- include/h264_source_filter.h
+++ include/h264_source_filter.h
@@ -1,10 +1,19 @@
 #ifndef _H264_SOURCE_FILTER_H_
 #define _H264_SOURCE_FILTER_H_
+#include "../include/rtp/depacketizer.h"
+#include "../include/RtpPacket.h"
+#include <list>
 
 class H264SourceFilter {
   public:
     H264SourceFilter();
     ~H264SourceFilter() { }
+    std::unique_ptr<RTPPacket> packet;
+    //TODO there should ideally be multiple input queues
+    //each associated with an SSRC (esp. in video conferencing)
+    //For now, deal with a single SSRC
+    std::list<RTPPacket> input_queue;
+    Depacketizer _depacketizer;
 };
 
 #endif
---
+++ src/h264_source_filter.cpp
@@ -0,0 +1,3 @@
+#include "../include/h264_source_filter.h"
+
+H264SourceFilter::H264SourceFilter() { }
