7d33a49c39d80635c1d9990ba4ca6f3f0efdbdfd
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-06-04 23:29:02 +0530

    IO muxer and RTP socket should be a part of source filter

--- include/h264_source_filter.h
+++ include/h264_source_filter.h
@@ -1,19 +1,24 @@
 #ifndef _H264_SOURCE_FILTER_H_
 #define _H264_SOURCE_FILTER_H_
 #include "../include/rtp/depacketizer.h"
+#include "../include/transport/UdpSocket.h"
 #include "../include/RtpPacket.h"
+#include "../libcircinus/include/AbstractIOMuxer.h"
 #include <list>
+#include <memory>
 
-class H264SourceFilter {
+class H264RTPSourceFilter {
   public:
-    H264SourceFilter();
-    ~H264SourceFilter() { }
+    H264RTPSourceFilter(std::unique_ptr<AbstractIOMuxer> io_muxer, const std::string &ip, uint16_t data_port, uint16_t server_rtp_port);
+    ~H264RTPSourceFilter() { }
     std::unique_ptr<RTPPacket> packet;
     //TODO there should ideally be multiple input queues
     //each associated with an SSRC (esp. in video conferencing)
     //For now, deal with a single SSRC
     std::list<RTPPacket> input_queue;
     Depacketizer _depacketizer;
+    std::unique_ptr<UdpSocket> _video_rtp_socket;  // rtp data listener
+    std::unique_ptr<AbstractIOMuxer> _io_muxer;
 };
 
 #endif
--- src/h264_source_filter.cpp
+++ src/h264_source_filter.cpp
@@ -1,3 +1,12 @@
 #include "../include/h264_source_filter.h"
 
-H264SourceFilter::H264SourceFilter() { }
+H264RTPSourceFilter::H264RTPSourceFilter(std::unique_ptr<AbstractIOMuxer> io_muxer, const std::string &ip, uint16_t data_port, uint16_t server_rtp_port) {
+  _io_muxer = std::move(io_muxer);
+  _video_rtp_socket = std::unique_ptr<UdpSocket>(new UdpSocket());
+  _video_rtp_socket->set_blocking(false);
+  _video_rtp_socket->bind_to(ip, data_port);
+  // TODO if we dont do this, then we might not receive any packets
+  // Found that this is used to overcome NAT traversal problem where we are behind a NAT
+  // and so the server's streams can't reach us
+  _video_rtp_socket->send_to(ip, server_rtp_port, "Îúíþ");
+}
