a49bca69be5fc14eeaca8cadd5fbb8fdff4449fb
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-06-06 23:25:47 +0530

    Fix include guard

--- include/filters/ISourceFilter.h
+++ include/filters/ISourceFilter.h
@@ -1,9 +1,9 @@
-#ifdef _ISOURCE_FILTER_H
-#define  _ISOURCE_FILTER_H
+#ifndef _ISOURCE_FILTER_H
+#define _ISOURCE_FILTER_H
 
 struct ISourceFilter {
-  virtual ~ISourceFilter() { }
-  virtual void get_next_frame(unsigned char* buffer) = 0;
+  virtual ~ISourceFilter() {}
+  virtual void get_next_frame(unsigned char *buffer) = 0;
 };
 
 #endif
