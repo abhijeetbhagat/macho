9c467290266c5acb2b65f680f9140d025d8a731a
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-06-06 23:28:05 +0530

    Include ISourceFilter header and make IO muxer optional

--- include/filters/h264_source_filter.h
+++ include/filters/h264_source_filter.h
@@ -1,27 +1,30 @@
 #ifndef _H264_SOURCE_FILTER_H_
 #define _H264_SOURCE_FILTER_H_
+#include "../../include/RtpPacket.h"
 #include "../../include/rtp/depacketizer.h"
 #include "../../include/transport/UdpSocket.h"
-#include "../../include/RtpPacket.h"
 #include "../../libcircinus/include/AbstractIOMuxer.h"
+#include "ISourceFilter.h"
 #include <list>
 #include <memory>
 
 class H264RTPSourceFilter : public ISourceFilter {
-  public:
-    H264RTPSourceFilter(std::unique_ptr<AbstractIOMuxer> io_muxer, const std::string &ip, uint16_t data_port, uint16_t server_rtp_port);
-    ~H264RTPSourceFilter();
-    void get_next_frame(unsigned char* buffer);
+public:
+  H264RTPSourceFilter(const std::string &ip, uint16_t data_port,
+                      uint16_t server_rtp_port,
+                      std::unique_ptr<AbstractIOMuxer> io_muxer = nullptr);
+  ~H264RTPSourceFilter();
+  void get_next_frame(unsigned char *buffer);
 
-  private:
-    std::unique_ptr<RTPPacket> packet;
-    //TODO there should ideally be multiple input queues
-    //each associated with an SSRC (esp. in video conferencing)
-    //For now, deal with a single SSRC
-    std::list<RTPPacket> input_queue;
-    Depacketizer _depacketizer;
-    std::unique_ptr<UdpSocket> _video_rtp_socket;  // rtp data listener
-    std::unique_ptr<AbstractIOMuxer> _io_muxer;
+private:
+  std::unique_ptr<RtpPacket> packet;
+  // TODO there should ideally be multiple input queues
+  // each associated with an SSRC (esp. in video conferencing)
+  // For now, deal with a single SSRC
+  std::list<RtpPacket> input_queue;
+  Depacketizer _depacketizer;
+  std::unique_ptr<UdpSocket> _video_rtp_socket; // rtp data listener
+  std::unique_ptr<AbstractIOMuxer> _io_muxer;
 };
 
 #endif
